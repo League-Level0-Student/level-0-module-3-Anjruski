fb61d5625098609a4fcb38d87295a2d582c5a675
catsrock <league@WTS14-4.attlocal.net>
2018-09-30 15:46:09 -0700

    Day I don't know

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -4,30 +4,30 @@ package extra;
 
 import javax.swing.JOptionPane;
 
-public class BananaQuiz
-{
-	
-	public static void main(String[] args)
-	{
-		//1. ask the user if they like bananas
+public class BananaQuiz {
+
+	public static void main(String[] args) {
+		// 1. ask the user if they like bananas
 		String answer = JOptionPane.showInputDialog("Do you like bananas?");
-		//2. if they say no, 
-		if (answer .equals("no")) {
+		// 2. if they say no,
+		if (answer.equals("no")) {
 			JOptionPane.showMessageDialog(null, "You are crazy!");
 		}
-			//tell them they are crazy 
-			//and end quiz
-		//3. if they say yes
-		if (answer .equals("yes")) {
+		// tell them they are crazy
+		// and end quiz
+		// 3. if they say yes
+		else if (answer.equals("yes")) {
 			String Hobby = JOptionPane.showInputDialog("what is your favorite hobby?");
 			JOptionPane.showMessageDialog(null, Hobby + "is better with bananas");
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhHHhhhHHHhhhHHhhhhHHHhhhhHHHhhhHHHhhhhHHHhhhHHHHhhhhHHHHHHHHHHHHHHHHHHHHHhhhhhHHHHHHHHHHHHhhhhHHHhhhhHHhhhhHHhhhhhHHhhhhHHHhhhhHHHHhhHHHHHHHHHHHHhhhhHHHhhhhHHhhhhHHhhhhHHhhhHHHhhhHHHhhhHHhhhHHHhhhHHHhhhhHHHhhhhhHHHhhhHHHhhhhhHHHhHHHHHHhhHHHHhhhhHHHhhhhHHHHhhhhhHHhhhhhhhHHHHhhhhHHHhhhhhHHHHhhhHhhHHHHhh");
 		}
-		//	ask them what is their favorite hobby
-		//	show a pop up that says "<your hobby> is much better with bananas!"
+		// ask them what is their favorite hobby
+		// show a pop up that says "<your hobby> is much better with bananas!"
 
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
-	
+		// 4. OPTIONAL: if they say something other than “yes�? or “no�?
+		// show a pop up that says “You are bananas!�?
+
 	}
-
 }
